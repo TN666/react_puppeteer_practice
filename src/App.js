331418c56b728de2def796bd2a9c57edd1d9ff9a
Ting <tingli@softnext.com.tw>
2022-08-23 10:07:45 +0800

    feat(React): add a simple progress bar app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      percent:"0%"
+    }
+    this.changePercent=this.changePercent.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  changePercent(p){
+    this.setState({percent:p})
+  }
 
+  controlButton() {
+    var output=[];
+    for(let i=25;i<=125;i+=25) {
+      if (i === 125) {
+        output.push(<button onClick={() => this.changePercent("0%")}>reset</button>);
+        return output;
+      }
+      output.push(<button onClick={() => this.changePercent(`${i}%`)}>{i}%</button>);
+    }
+    return output;
+  }
+
+  render(){
+        return(
+          <div>
+            <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"500px",height:"30px",borderRadius:"10px"}}>
+              <div className="progress-bar" style={{backgroundColor:"#fe5196",width:this.state.percent,height:"100%",borderRadius:"10px"}}></div>
+            </div>
+            { this.controlButton() }
+          </div>
+        );
+    }
+  }
 export default App;
