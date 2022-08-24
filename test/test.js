a459a05cd4a6070c141a74a80f9a6d4375993319
Ting <tingli@softnext.com.tw>
2022-08-24 15:03:09 +0800

    test(e2e): add a test to practice puppeteer waitForFunction

--- test/test.js
+++ test/test.js
@@ -4,25 +4,30 @@ beforeAll(async () => { await page.goto(URL); });
 
 describe('Button Test', () => { 
 
-test('25% button', async () => {
-  await testButton(25)
-});
+  test('25% button', async () => {
+    await testButton(25)
+  });
 
-test('50% button', async () => {
-  await testButton(50)
-});
+  test('50% button', async () => {
+    await testButton(50)
+  });
 
-test('75% button', async () => {
-  await testButton(75)
-});
+  test('75% button', async () => {
+    await testButton(75)
+  });
 
-test('100% button', async () => {
-  await testButton(100)
-});
+  test('100% button', async () => {
+    await testButton(100)
+  });
 
-test('reset button', async () => {
-  await testButton(0)
-});
+  test('reset button', async () => {
+    await testButton(0)
+  });
+
+  test('delayed button', async () => {
+    await page.click('#delay-100');
+    await page.waitForFunction(() => document.querySelector('.progress-bar').textContent === '100%');
+  },10000);
 
 });
 
